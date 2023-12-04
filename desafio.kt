797626b75108782b358d21c48793bdba14b1910f
MaiaJrDev <88292112+MaiaJrDev@users.noreply.github.com>
2023-12-04 17:28:47 -0300

    implementando classes do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,31 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val name: String, val age: Int, email: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuarios: List<Usuario>) {
+        for (usuario in usuarios) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.name} matriculado com sucesso!")
+        }
+        println("Quantidade de usuarios matriculados para a formação $nome : ${inscritos.size}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val firstContent = ConteudoEducacional("kotlin", nivel = Nivel.BASICO)
+    val secondContent = ConteudoEducacional("JavaScript", 80, Nivel.INTERMEDIARIO)
+    val thirdContent = ConteudoEducacional("Java", 120, Nivel.AVANCADO)
+    val formacao = Formacao("Desenvolvedor Master", listOf(firstContent, secondContent, thirdContent))
+    
+    val user1 = Usuario("Roberto Maia", 29, "roberto@dio.me")
+    val user2 = Usuario("Vanderlei", 34, "van@dio.me")
+    val user3 = Usuario("Josefa", 19, "jose@dio.me")
+    formacao.matricular(listOf(user1, user2, user3))
+    println(formacao.inscritos)
 }
